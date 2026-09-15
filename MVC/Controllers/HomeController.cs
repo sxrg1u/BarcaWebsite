abbbca2e7a31db8f468bbf55e03ae6b79bbc7ff1
sxrg1u <sergiu.bolohan@outlook.com>
2026-09-15 10:45:15 +0200

    not found seite und fonts apgelegt

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -37,8 +37,14 @@ namespace MVC.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("Home/Error/{statusCode?}")]
+        public IActionResult Error(int? statusCode = null)
         {
+            if (statusCode == 404)
+            {
+                return View("NotFound");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
--- MVC/Program.cs
+++ MVC/Program.cs
@@ -13,6 +13,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
